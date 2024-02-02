60fbd41f621d39bc9293eac847c33ad6f49af5e9
Hyo Bin <a032875@gmail.com>
2024-02-02 23:35:27 +0900

    NotFound페이지 구현

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,10 +1,35 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "../App.jsx";
-import "../index.css";
-// import "./reset.css";
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Footer from "../components/layout/Footer";
+import Nav from "../components/layout/Nav";
+import Slider from "../components/common/Slider";
+import ItemList from "../components/common/ItemList";
+import ProductHeader from "../components/products/ProductHeader";
+import { ClothingLimitLoad } from "../components/products/ProductsList";
+import { DigitalLoad } from "../components/products/ProductsList";
+import { AccessoriesLoad } from "../components/products/ProductsList";
+const Main = () => {
+  return (
+    <div className="w-screen">
+      <Nav />
+      <Slider />
+      <ItemList />
+      <div className="overflow-x-auto">
+        <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mt-10 xl:container mx-auto">
+          <ProductHeader pageTitle={"패션"} />
+          <ClothingLimitLoad />
+        </section>
+      </div>
+      <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mt-10 xl:container mx-auto">
+        <ProductHeader pageTitle={"액세서리"} />
+        <DigitalLoad />
+      </section>
+      <section className="pt-6 lg:pt-12 pb-4 lg:pb-8 px-4 xl:px-2 mt-10 xl:container mx-auto">
+        <ProductHeader pageTitle={"디지털"} />
+        <AccessoriesLoad />
+      </section>
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
